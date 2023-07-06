84aaeb37e233993d6d720472773576c04ae0664e
XenoHype <xpertminer01@gmail.com>
2023-07-06 19:26:14 -0400

    Initial Commit

---
+++ Assets/Scripts/Motion.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Com.FluffyFoxFederation.VirtualPigMiner
+
+{
+    public class Motion : MonoBehaviour
+    {
+
+        public float speed;
+        public float sprintModifier;
+        public float jumpForce;
+        public Camera normalCam;
+        public Transform groundDetector;
+        public LayerMask ground;
+        
+
+        private Rigidbody rig;
+        
+        private float baseFOV;
+        private float sprintFOVModifier = 1.25f;
+        
+        private void Start()
+        {
+            baseFOV = normalCam.fieldOfView;
+            Camera.main.enabled = false;
+            rig = GetComponent<Rigidbody>();
+        }
+
+        
+        void FixedUpdate()
+        {
+            //Axis
+            float t_hmove = Input.GetAxisRaw("Horizontal");
+            float t_vmove = Input.GetAxisRaw("Vertical");
+            
+            //Controls
+            bool sprint = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
+            bool jump = Input.GetKey(KeyCode.Space);
+            
+            
+            //States
+            bool isGrounded = (Physics.Raycast(groundDetector.position, Vector3.down, 0.1f, ground));
+            bool isJumping = jump && isGrounded;
+            bool isSprinting = sprint && t_vmove > 0 && !isJumping && isGrounded;
+            
+            //Jumping
+            if (isJumping)
+                
+            {
+                rig.AddForce(Vector3.up * jumpForce);
+            }
+            
+            
+            //Movement
+            Vector3 t_direction = new Vector3(t_hmove, 0, t_vmove);
+            t_direction.Normalize();
+
+            float t_adjustmentSpeed = speed;
+            if (isSprinting) t_adjustmentSpeed *= sprintModifier;
+
+            Vector3 t_targetVelocity = transform.TransformDirection(t_direction) * t_adjustmentSpeed * Time.deltaTime;
+            t_targetVelocity.y = rig.velocity.y;
+            rig.velocity = t_targetVelocity;
+            
+            //Field of View
+            if (isSprinting)
+            {
+                normalCam.fieldOfView = Mathf.Lerp(normalCam.fieldOfView, baseFOV * sprintFOVModifier, Time.deltaTime * 8f);
+            }
+            else
+            { 
+                normalCam.fieldOfView = Mathf.Lerp(normalCam.fieldOfView, baseFOV, Time.deltaTime * 8f);;
+            }
+        }
+    }
+}
